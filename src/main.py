586af25c6cdfd5468132cbcada10d5d50ab5a0c3
Ike <50747621+Ike-li@users.noreply.github.com>
2023-12-15 14:19:53 +0800

    .

--- src/main.py
+++ src/main.py
@@ -7,33 +7,45 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# TODO 这个函数名想一下
 def github(developer_work_dir, token, proxy):
-    url = "https://api.github.com/user/repos?type=all&sort=updated"
+    url = "https://api.github.com/user/repos"
 
-    payload = {}
     headers = {"Authorization": f"Bearer {token}"}
-    response = requests.request("GET", url, headers=headers, data=payload)
 
-    if "message" in response.json():
-        raise "Token is expire"
+    # Fetch all repositories by handling pagination
+    all_repos = []
+    page = 1
+    per_page = 100  # Adjust the number of repos per page as per GitHub's API limit
 
-    for repo in response.json():
+    while True:
+        params = {"page": page, "per_page": per_page}
+        response = requests.get(url, headers=headers, params=params)
+
+        if response.status_code == 200:
+            repos = response.json()
+            if len(repos) == 0:
+                break
+            all_repos.extend(repos)
+            page += 1
+        else:
+            raise Exception(f"Failed to fetch repositories:{response.json()}. Status code: {response.status_code}")
+
+    for repo in all_repos:
         repo_name = repo["name"]
         clone_url = repo["clone_url"]
 
         if os.path.exists(f"{developer_work_dir}/{repo_name}"):
             print(repo_name + " 已存在，即将切换到 master 进行更新")
-            _shell(
-                f" cd {developer_work_dir}{repo_name} && {proxy} && git switch master && git pull origin master"
-            )
-
-            if os.path.exists(f"{developer_work_dir}/{repo_name}/docker"):
-                print(repo_name + " 存在 docker 目录，即将启动")
-                _shell(
-                    f"cd {developer_work_dir}/{repo_name}/docker && docker-compose up -d"
-                )
-                print(repo_name + " docker 启动完成")
+            # _shell(
+            #     f" cd {developer_work_dir}{repo_name} && {proxy} && git switch master && git pull origin master"
+            # )
+            #
+            # if os.path.exists(f"{developer_work_dir}/{repo_name}/docker"):
+            #     print(repo_name + " 存在 docker 目录，即将启动")
+            #     _shell(
+            #         f"cd {developer_work_dir}/{repo_name}/docker && docker-compose up -d"
+            #     )
+            #     print(repo_name + " docker 启动完成")
         else:
             print(repo_name + " 不存在， 即将 clone " + clone_url)
             _shell(
@@ -54,6 +66,7 @@ def _seek_developer_work_dir():
         "/Users/one/project/",
         "/Users/viktor/project/",
         "/Users/leo/PycharmProjects/",
+        '/Users/johnnyli/project',
     ]
 
     for work_dir in developer_work_dirs:
