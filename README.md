a2fde131fc3918434bbd128276f4d37ce9793d40
Ike-li <viktorl@163.com>
2024-08-29 18:19:47 +0800

    .

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # download_github_repo
+使用 GitHub API 获取用户的仓库列表, 下载所有你拥有权限的 GitHub 仓库到本地
+
+Retrieve a list of user repositories using the GitHub API and download all the GitHub repositories you have access to locally.
--- src/main.py
+++ src/main.py
@@ -1,69 +1,19 @@
 import os
 import subprocess
-
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-def github(developer_work_dir, token, proxy):
-    url = "https://api.github.com/user/repos"
-
-    headers = {"Authorization": f"Bearer {token}"}
-
-    # Fetch all repositories by handling pagination
-    all_repos = []
-    page = 1
-    per_page = 100  # Adjust the number of repos per page as per GitHub's API limit
-
-    while True:
-        params = {"page": page, "per_page": per_page}
-        response = requests.get(url, headers=headers, params=params)
-
-        if response.status_code == 200:
-            repos = response.json()
-            if len(repos) == 0:
-                break
-            all_repos.extend(repos)
-            page += 1
-        else:
-            raise Exception(f"Failed to fetch repositories:{response.json()}. Status code: {response.status_code}")
-
-    for repo in all_repos:
-        repo_name = repo["name"]
-        clone_url = repo["clone_url"]
-        if repo_name in ["code", "diagnosis", "health_care"]:
-            # 跳过大文件
-            continue
-
-        if os.path.exists(f"{developer_work_dir}/{repo_name}"):
-            print(repo_name + " 已存在，即将切换到 master 进行更新")
-            # _shell(
-            #     f" cd {developer_work_dir}{repo_name} && {proxy} && git switch master && git pull origin master"
-            # )
-            #
-            # if os.path.exists(f"{developer_work_dir}/{repo_name}/docker"):
-            #     print(repo_name + " 存在 docker 目录，即将启动")
-            #     _shell(
-            #         f"cd {developer_work_dir}/{repo_name}/docker && docker-compose up -d"
-            #     )
-            #     print(repo_name + " docker 启动完成")
-        else:
-            print(repo_name + " 不存在， 即将 clone " + clone_url)
-            if proxy:
-                _shell(
-                    f"cd {developer_work_dir} && {proxy} && git clone {clone_url}")
-                # gh repo clone user_name/repo_name 可能会更快
-            else:
-                _shell(
-                    f"cd {developer_work_dir} && git clone {clone_url}")
-
-
 def _shell(cmd):
     print(">", cmd)
     result = subprocess.run(cmd, shell=True)
     return result.returncode
+
+
+def _cd(work_dir):
+    os.chdir(work_dir)
 
 
 def _seek_developer_work_dir():
@@ -85,8 +35,56 @@ def _seek_developer_work_dir():
     raise Exception("Developer work dir not found")
 
 
+def github(developer_work_dir, token, proxy=None):
+    url = "https://api.github.com/user/repos"
+
+    headers = {"Authorization": f"Bearer {token}"}
+
+    # Fetch all repositories by handling pagination
+    all_repos = []
+    page = 1
+    per_page = 100  # Adjust the number of repos per page as per GitHub's API limit
+
+    while True:
+        params = {"page": page, "per_page": per_page}
+        response = requests.get(url, headers=headers, params=params)
+
+        if response.status_code == 200:
+            repos = response.json()
+            if len(repos) == 0:
+                break
+            all_repos.extend(repos)
+            page += 1
+        else:
+            error_message = f"Failed to fetch repositories: {response.json()}. Status code: {response.status_code}"
+            raise Exception(error_message)
+
+    for repo in all_repos:
+        repo_name = repo["name"]
+        clone_url = repo["clone_url"]
+        if repo_name in ["code", "diagnosis", "health_care"]:
+            # 跳过大文件
+            continue
+
+        if os.path.exists(f"{developer_work_dir}/{repo_name}"):
+            print(repo_name + " 已存在，即将切换到 master 进行更新")
+            _cd(f"{developer_work_dir}/{repo_name}")
+            _shell(
+                f"{proxy} && git switch master && git pull origin master" if proxy else "git switch master && git pull origin master")
+        else:
+            print(repo_name + " 不存在， 即将 clone " + clone_url)
+            if proxy:
+                _shell(f"cd {developer_work_dir} && {proxy} && git clone {clone_url}")
+            else:
+                _shell(f"cd {developer_work_dir} && git clone {clone_url}")
+
+
 if __name__ == "__main__":
     token = os.getenv("TOKEN")
+    if not token:
+        raise Exception("Environment variable TOKEN is not set")
+
     proxy = os.getenv("PROXY")
+
     developer_work_dir = _seek_developer_work_dir()
     github(developer_work_dir, token, proxy)
